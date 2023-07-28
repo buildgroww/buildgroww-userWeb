e9eb9f7a39b0b48af7f8a331dbca11f9267d06ed
Nitish847235 <nitishpatwarabihar@gmail.com>
2023-07-28 21:09:27 +0530

    update by nitish

--- src/pages/company/Company.jsx
+++ src/pages/company/Company.jsx
@@ -1,13 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Block1 from '../../contents/company/block1/Block1'
 import Navbar from '../../layout/mainLayout/navbar/Navbar'
 import { Box } from '@mui/material'
+import { useDispatch, useSelector } from '../../redux/store/store'
+import { getCompanyData } from '../../redux/slices/company'
 function Company() {
+  const dispatch = useDispatch();
+  const [page,setPage] = useState(1);
+
+  const location = window.location.pathname.split('/')[1]
+  const search = window.location.pathname.split('/')[2]
+
+  const companyData = useSelector((state)=>state.company)
+
+
+  useEffect(() => {
+    
+    let fetchData = async()=>{
+      let query = {"$and":[{"address.city":{"$regex":`${location}`},"$options":"i"},{"$or":[{"name":{"$regex":`${search?search:''}`},"$options":"i"},{"title":{"$regex":`${search?search:''}`},"$options":"i"}]}]}
+      let sort = {"name":1}
+      dispatch(getCompanyData(query,sort,page))
+    }
+
+    fetchData();
+  }, [])
+  
+
   return (
     <>
     <Navbar menu={'block'} arrow={'none'} logo={'Build Groww'}/>
     <Box sx={{marginTop:"100px"}}>
-    <Block1/>
+    <Block1 companyData={companyData}/>
     </Box>
     </>
   )
--- src/redux/store/rootReducer.js
+++ src/redux/store/rootReducer.js
@@ -1,8 +1,10 @@
 import {reducer as authReducer} from "../slices/auth";
+import {reducer as companyReducer} from "../slices/company";
 
 import {combineReducers} from "@reduxjs/toolkit";
 
+
 export const rootReducer = combineReducers({
-   auth:authReducer, 
-  
+   auth:authReducer,
+   company:companyReducer 
 });
